33b2f82a44061782729b99e08b1ca0be377c9f03
Eugene Krotenko <eugene.krotenko@caspio.com>
2016-12-17 14:55:03 +0200

    removed waste java files

--- src/com/ekrotenko/GameDemo.java
+++ src/com/ekrotenko/GameDemo.java
@@ -4,13 +4,14 @@ import com.ekrotenko.input.ConstantInput;
 import com.ekrotenko.input.FieldInput;
 import com.ekrotenko.output.ConsoleOutput;
 import com.ekrotenko.output.FieldOutput;
+import com.ekrotenko.input.ConsoleInput;
 
 public class GameDemo {
 
     public static void main(String[] args) {
 
-        FieldInput input = new ConstantInput(20,15,true);
-        //FieldInput input = new ConsoleInput(true);
+        //FieldInput input = new ConstantInput(20,15,true);
+        FieldInput input = new ConsoleInput(true);
         FieldOutput output = new ConsoleOutput();
 
         Game game = new Game(input, output);
